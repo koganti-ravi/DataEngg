c0696ca4d92d67e965dac71ccfef6fdb2735febd
Udaya Lakshmi Cherukuri <udayalakshmicherukuri@Udayas-iMac.attlocal.net>
2024-09-14 04:17:23 -0500

    terra

---
+++ terraform/maint.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+ region = "us-east-1"
+}
+
+resource "aws_instance" "ec2_example" {
+   
+   ami           = "ami-0182f373e66f89c85"
+   instance_type = "t2.micro"
+   
+   tags = {
+           Name = "Terraform EC2"
+   }
+}
